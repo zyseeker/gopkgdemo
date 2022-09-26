726e38fb9425b7db08af442f1497200989021c43
Steven Zhang <zhangst@vmware.com>
2022-09-26 09:36:11 +0800

    add steven

--- naming/random.go
+++ naming/random.go
@@ -4,7 +4,7 @@ import (
 	"math/rand"
 )
 
-var names = []string{"Mike", "Jack", "Tim"}
+var names = []string{"Mike", "Jack", "Tim", "Steven"}
 
 func GiveMeAName() string {
 	return names[rand.Int()%3]
